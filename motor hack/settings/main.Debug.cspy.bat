1b9d46058e0ae998bdb7e8892d086ac7b91b8efe
salviador <michelemancini2@tiscali.it>
2017-06-04 10:35:38 +0200

    Update version

--- motor hack/settings/main.Debug.cspy.bat
+++ motor hack/settings/main.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "C:\Users\michele\Desktop\HOWERBOARD HACK\weelchair motor hack\settings\main.Debug.general.xcl" --backend -f "C:\Users\michele\Desktop\HOWERBOARD HACK\weelchair motor hack\settings\main.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\michele\Desktop\HOWERBOARD HACK\weelchair motor hack\settings\main.Debug.general.xcl" --backend -f "C:\Users\michele\Desktop\HOWERBOARD HACK\weelchair motor hack\settings\main.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin\cspybat" -f "C:\Users\michele\Desktop\HOWERBOARD HACK\weelchair motor hack\settings\main.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\michele\Desktop\HOWERBOARD HACK\weelchair motor hack\settings\main.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "C:\Users\michele\Desktop\HOWERBOARD HACK\weelchair motor hack\settings\main.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\michele\Desktop\HOWERBOARD HACK\weelchair motor hack\settings\main.Debug.driver.xcl" 
 
 @echo off 
 :end
